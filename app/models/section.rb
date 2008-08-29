59fafe295dd0e6bece1443b02e7cebf0fb14631f
Brandon Zylstra <brandon.zylstra@gmail.com>
2008-08-29 01:16:54 -0400

    got section tabs to be generated dynamically from Section objects (and got those to work with some voodoo programming: I did rake db:rollback and rake db:migrate and all the problems went away with no code changes!  apparently sqlite hiccupped when it first created the table or something!) but I can't get the dynamic highlighting working for the current section, and haven't gotten the routing working for sections yet, or the finders for sections

---
+++ app/models/section.rb
@@ -0,0 +1,14 @@
+class Section < ActiveRecord::Base
+  
+  def is_tab_check
+    if self.is_tab
+      "&#10003;" # checkmark HTML entity... TODO: test for support, this might need to be changed
+    end
+  end
+  
+  def highlight_tab
+    if self == @section # ???? TODO: get this working so I can highlight one link
+      'class="activelink"'
+    end
+  end
+end
