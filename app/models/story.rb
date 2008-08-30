6c216095ef51dfa60c3391e71e4013273310e9c3
Brandon Zylstra <brandon.zylstra@gmail.com>
2008-08-29 23:18:42 -0400

    commented out association with :section in story.rb and the bug disappeared (under the rug for now) and commented out date validation since it's complaining about valid dates, and tested the other validation in story.rb

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,19 +1,25 @@
 class Story < ActiveRecord::Base
   
-  belongs_to :section # section_id
+  #belongs_to :section # section_id
+  # TODO: uncomment the line above... it currently causes a problem saving stories.  the error message is
+  # Section(#31380870) expected, got String(#113650)
   
   has_many :photo_placements
   has_many :photos, :through => :photo_placements
   
   validates_presence_of		:title, :body, :intro, :extract #, <<<< CHANGE_TO :excerpt # I misnamed it accidentally...
+  validates_presence_of :section, :author
 	validates_uniqueness_of :title, :on => :create, :message => "must be unique. A story already exists with that title."
-  
+
+
   require_dependency 'date_validator'
 
-  validates_dates :publish_at,
-                  :from => '1 Jan 2000',
-                  :to => '31 Dec 2020',
-                  :allow_nil => true
+
+  # this is currently stopping VALID dates from working... TODO
+  # validates_dates :publish_at,
+  #                 :from => '1 Jan 2000',
+  #                 :to => '31 Dec 2020',
+  #                 :allow_nil => true
                   # Date.today seems to restrictive: someone *might* need to mark something for the past,
                   # and it is certainly useful to post in the future, although I don't *yet* wait to publish it...
                   # but the idea of the field name not being past tense is that it is FORWARD-LOOKING
