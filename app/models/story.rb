659a8d8160b405da94bee672ca1633c55467b868
Brandon Zylstra <brandon.zylstra@gmail.com>
2008-08-29 22:05:07 -0400

    added validation to Story but can't test because of a bug related to sections 'Section(#31380870) expected, got String(#113650)' which I think has to do with the association betwen Story and Section and the fact that I have a section field in story...

--- app/models/story.rb
+++ app/models/story.rb
@@ -5,13 +5,15 @@ class Story < ActiveRecord::Base
   has_many :photo_placements
   has_many :photos, :through => :photo_placements
   
+  validates_presence_of		:title, :body, :intro, :extract #, <<<< CHANGE_TO :excerpt # I misnamed it accidentally...
+	validates_uniqueness_of :title, :on => :create, :message => "must be unique. A story already exists with that title."
   
   require_dependency 'date_validator'
 
-  # validates_dates :publish_at,
-  #                 :from => '1 Jan 2000',
-  #                 :to => '31 Dec 2020',
-  #                 :allow_nil => true
+  validates_dates :publish_at,
+                  :from => '1 Jan 2000',
+                  :to => '31 Dec 2020',
+                  :allow_nil => true
                   # Date.today seems to restrictive: someone *might* need to mark something for the past,
                   # and it is certainly useful to post in the future, although I don't *yet* wait to publish it...
                   # but the idea of the field name not being past tense is that it is FORWARD-LOOKING
