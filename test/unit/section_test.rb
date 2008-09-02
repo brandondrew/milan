d99584589e050fb6c997261e2bb1f81a06665ade
Brandon Zylstra <brandon.zylstra@gmail.com>
2008-09-02 19:59:50 -0400

    added some tests...

--- test/unit/section_test.rb
+++ test/unit/section_test.rb
@@ -1,8 +1,28 @@
 require 'test_helper'
 
 class SectionTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :sections
+  
+
+  
+  def test_all_sections
+    # sections exist
+    all_sections 
+    assert_response :success
+  end      
+
+  def test_current_section
+    assert_valid(current_section)
   end
-end
+  
+  def test_current_section_name
+    # is this one even worth doing?  I suppose maybe
+    assert_equal current_section.name current_section_name    
+  end
+  
+  # def test_tab_style(tab_section)  
+  #   assert true
+  # end
+  
+  
+end        
--- test/unit/story_test.rb
+++ test/unit/story_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StoryTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
+  # def test_truth
+  #   assert true
+  # end
 end
