2ad8fca0a9176954b39ba906568dce88982818d1
Brandon Zylstra <brandon.zylstra@gmail.com>
2008-09-03 04:53:07 -0400

    cleaned up unit tests

--- test/unit/section_test.rb
+++ test/unit/section_test.rb
@@ -2,27 +2,6 @@ require 'test_helper'
 
 class SectionTest < ActiveSupport::TestCase
   fixtures :sections
-  
 
-  
-  def test_all_sections
-    # sections exist
-    all_sections 
-    assert_response :success
-  end      
-
-  def test_current_section
-    assert_valid(current_section)
-  end
-  
-  def test_current_section_name
-    # is this one even worth doing?  I suppose maybe
-    assert_equal current_section.name current_section_name    
-  end
-  
-  # def test_tab_style(tab_section)  
-  #   assert true
-  # end
-  
   
 end        
